6a3cae7bc72f53a3b0d6dd941daf67b067079889
leonardchiu <chiu.leonard@gmail.com>
2020-07-19 15:17:25 +0800

    Update database js file for production postgresql

--- config/database.js
+++ config/database.js
@@ -1,15 +1,55 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => {
+	if (env('NODE_ENV') === 'development') {
+		return {
+			defaultConnection: 'default',
+			connections: {
+				default: {
+					connector: 'bookshelf',
+					settings: {
+						client: 'sqlite',
+						filename: env('DATABASE_FILENAME', '.tmp/data.db')
+					},
+					options: {
+						useNullAsDefault: true
+					}
+				}
+			}
+		};
+	} else {
+		return {
+			defaultConnection: 'default',
+			connections: {
+				default: {
+					connector: 'bookshelf',
+					settings: {
+						client: 'postgres',
+						host: env('DATABASE_HOST', '127.0.0.1'),
+						port: env('DATABASE_PORT', 5432),
+						database: env('DATABASE_NAME', 'strapi'),
+						username: env('DATABASE_USERNAME', ''),
+						password: env('DATABASE_PASSWORD', '')
+					},
+					options: {
+						ssl: false
+					}
+				}
+			}
+		};
+	}
+};
