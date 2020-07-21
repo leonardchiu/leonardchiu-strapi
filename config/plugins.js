7134ae09bb5f0e6ad69971697b7774f8d2f6f68e
leonardchiu <chiu.leonard@gmail.com>
2020-07-21 23:26:43 +0800

    Change cloudinary cloud name

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,25 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   upload: {
-//     provider: "cloudinary",
-//     providerOptions: {
-//       cloud_name: env("leonardchiu-cloudinary"),
-//       api_key: env("322351532493493"),
-//       api_secret: env("x0wBMYXHAAu7UF6hm55M8vPbUVM"),
-//     },
-//   },
-//   // ...
-// });
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: "${process.env.leonardchiu-cloudinary || ''}",
-      api_key: "${process.env.322351532493493 || ''}",
-      api_secret: "${process.env.0wBMYXHAAu7UF6hm55M8vPbUVM || ''}",
+      cloud_name: env("leonardchiucloudinary"),
+      api_key: env("322351532493493"),
+      api_secret: env("x0wBMYXHAAu7UF6hm55M8vPbUVM"),
     },
-  };
-} else {
-  module.exports = {};
-}
+  },
+  // ...
+});
+
+// if (process.env.NODE_ENV === "development") {
+//   module.exports = {
+//     provider: "cloudinary",
+//     providerOptions: {
+//       cloud_name: "${process.env.leonardchiucloudinary || ''}",
+//       api_key: "${process.env.322351532493493 || ''}",
+//       api_secret: "${process.env.0wBMYXHAAu7UF6hm55M8vPbUVM || ''}",
+//     },
+//   };
+// } else {
+//   module.exports = {};
+// }
