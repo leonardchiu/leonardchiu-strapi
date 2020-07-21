70e0b447b01502a8c41139be4db5c47872f069b0
leonardchiu <chiu.leonard@gmail.com>
2020-07-21 18:38:40 +0800

    If statement for cloudinary settings for production ENV

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,25 @@
-module.exports = ({ env }) => ({
-  // ...
-  upload: {
+// module.exports = ({ env }) => ({
+//   // ...
+//   upload: {
+//     provider: "cloudinary",
+//     providerOptions: {
+//       cloud_name: env("leonardchiu-cloudinary"),
+//       api_key: env("322351532493493"),
+//       api_secret: env("x0wBMYXHAAu7UF6hm55M8vPbUVM"),
+//     },
+//   },
+//   // ...
+// });
+
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: env("leonardchiu-cloudinary"),
-      api_key: env("322351532493493"),
-      api_secret: env("x0wBMYXHAAu7UF6hm55M8vPbUVM"),
+      cloud_name: "${process.env.leonardchiu-cloudinary || ''}",
+      api_key: "${process.env.322351532493493 || ''}",
+      api_secret: "${process.env.0wBMYXHAAu7UF6hm55M8vPbUVM || ''}",
     },
-  },
-  // ...
-});
+  };
+} else {
+  module.exports = {};
+}
