c3c4fa2eb41485921ad1b7c55f6cbb80c7629ad1
leonardchiu <chiu.leonard@gmail.com>
2020-07-21 10:38:59 +0800

    Setup cloudinary plugin

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("leonardchiu-cloudinary"),
+      api_key: env("322351532493493"),
+      api_secret: env("x0wBMYXHAAu7UF6hm55M8vPbUVM"),
+    },
+  },
+  // ...
+});
